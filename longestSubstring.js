c6930cb5f85324111bd57b38f5fd40455c0c7f15
Calberk <park.edmund@gmail.com>
2018-11-13 23:13:35 -0800

    Completed Caeser-Cypher

---
+++ longestSubstring.js
@@ -0,0 +1,13 @@
+function longestSubstring(s){
+    const sArray = s.split('')
+    const longestSubstring = [];
+    while(sArray.length){
+        const currentStr = [""];
+        if(currentStr.indexOf(s[i])===-1){
+            currentStr.push(s[i])
+            i++
+        }else{
+            
+        }
+    }
+}
